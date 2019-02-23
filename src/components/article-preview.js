8624ff5633bcaea970bd66dd6bf7b2bf689153eb
Daan van der Zwaag <daan@devign.it>
2019-02-23 23:50:28 +0100

    test push

--- src/components/article-preview.js
+++ src/components/article-preview.js
@@ -1,20 +1,18 @@
-import React from 'react'
-import { Link } from 'gatsby'
-import Img from 'gatsby-image'
-
-import styles from './article-preview.module.css'
+import React from "react";
+import { Link } from "gatsby";
+import Img from "gatsby-image";
 
 export default ({ article }) => (
-  <div className={styles.preview}>
+  <div>
     <Img alt="" fluid={article.heroImage.fluid} />
-    <h3 className={styles.previewTitle}>
+    <h3>
       <Link to={`/blog/${article.slug}`}>{article.title}</Link>
     </h3>
     <small>{article.publishDate}</small>
     <p
       dangerouslySetInnerHTML={{
-        __html: article.description.childMarkdownRemark.html,
+        __html: article.description.childMarkdownRemark.html
       }}
     />
   </div>
-)
+);
