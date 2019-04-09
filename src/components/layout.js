e4f427b4ee7f2424bc6e32810fe8188cef5935db
Daan van der Zwaag <daan@devign.it>
2019-04-09 23:55:27 +0200

    add: structure to main layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,13 +1,28 @@
-import React from 'react'
-import Container from './MainContainer'
+import React from "react";
+import get from "lodash/get";
+import PageContent from "./PageContent";
+import Navigation from "./Navigation";
+import Helmet from "react-helmet";
+import Hero from "./Hero";
+import { changeDocumentTitle } from "./SiteTitle";
 
 export default class LayoutHome extends React.Component {
+  componentDidMount() {
+    changeDocumentTitle();
+    // quoteAnimation();
+  }
+
   render() {
-    const { children } = this.props
+    const siteTitle = get(this, "props.data.site.siteMetadata.title");
+    const { children } = this.props;
+
     return (
-      <Container>
-        {children}
-      </Container>
-    )
+      <div className="layoutHome">
+        <Helmet title={siteTitle} />
+        <Hero />
+        <Navigation />
+        <PageContent>{children}</PageContent>
+      </div>
+    );
   }
 }
