726c4db7420cfae73e1e62c1ec0feb330f82033e
Daan van der Zwaag <daan@devign.it>
2019-04-09 23:51:32 +0200

    🧹 blog modules

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,42 +1,40 @@
-import React from 'react'
-import { Link, graphql } from 'gatsby'
-import get from 'lodash/get'
-import Helmet from 'react-helmet'
+import React from "react";
+import { Link, graphql } from "gatsby";
+import get from "lodash/get";
+import Helmet from "react-helmet";
 // import styles from './blog.module.css'
-import Layout from "../components/layout"
-import ArticlePreview from '../components/article-preview'
+import Layout from "../components/layout";
+import ArticlePreview from "../components/article-preview";
 
 class BlogIndex extends React.Component {
   render() {
-    const siteTitle = get(this, 'props.data.site.siteMetadata.title')
-    const posts = get(this, 'props.data.allContentfulBlogPost.edges')
+    const siteTitle = get(this, "props.data.site.siteMetadata.title");
+    const posts = get(this, "props.data.allContentfulBlogPost.edges");
 
     return (
-      <Layout location={this.props.location} >
-        <div style={{ background: '#fff' }}>
+      <Layout location={this.props.location}>
+        <div>
           <Helmet title={siteTitle} />
-          <div>
-            Blog
-          </div>
+          <div>Blog</div>
           <div className="wrapper">
             <h2 className="section-headline">Recent articles</h2>
-            <ul className="article-list">
+            <section className="article-list">
               {posts.map(({ node }) => {
                 return (
-                  <li key={node.slug}>
+                  <article key={node.slug}>
                     <ArticlePreview article={node} />
-                  </li>
-                )
+                  </article>
+                );
               })}
-            </ul>
+            </section>
           </div>
         </div>
       </Layout>
-    )
+    );
   }
 }
 
-export default BlogIndex
+export default BlogIndex;
 
 export const pageQuery = graphql`
   query BlogIndexQuery {
@@ -61,4 +59,4 @@ export const pageQuery = graphql`
       }
     }
   }
-`
+`;
--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -7,14 +7,14 @@ import Layout from '../components/layout'
 
 import heroStyles from '../components/hero.module.css'
 
-class BlogPostTemplate extends React.Component {
+export default class BlogPostTemplate extends React.Component {
   render() {
     const post = get(this.props, 'data.contentfulBlogPost')
     const siteTitle = get(this.props, 'data.site.siteMetadata.title')
 
     return (
       <Layout location={this.props.location} >
-        <div style={{ background: '#fff' }}>
+        <div>
           <Helmet title={`${post.title} | ${siteTitle}`} />
           <div className={heroStyles.hero}>
             <Img className={heroStyles.heroImage} alt={post.title} fluid={post.heroImage.fluid} />
@@ -40,7 +40,6 @@ class BlogPostTemplate extends React.Component {
   }
 }
 
-export default BlogPostTemplate
 
 export const pageQuery = graphql`
   query BlogPostBySlug($slug: String!) {
