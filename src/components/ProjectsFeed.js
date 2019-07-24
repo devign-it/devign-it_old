0122ec7feba477b4917c9abed065663c9926408d
rubennijhuis <contact@rubenswebsite.com>
2019-07-24 15:46:26 +0200

    🔨fix: navbar to intended behavior

--- src/components/ProjectsFeed.js
+++ src/components/ProjectsFeed.js
@@ -1,32 +1,30 @@
-import React from 'react'
-import ProjectItem from './ProjectItem'
-import Swiper from 'swiper'
+import React from "react";
+import ProjectItem from "./ProjectItem";
+import Swiper from "swiper";
 
 export const initializeSwiper = () =>
-  new Swiper('.swiper-container', {
-    slidesPerView: 1,
-    spaceBetween: 50,
-    loop: true,
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'fraction',
-    },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  })
+    new Swiper(".swiper-container", {
+        slidesPerView: 1,
+        spaceBetween: 50,
+        loop: true,
+        pagination: {
+            el: ".swiper-pagination",
+            type: "fraction",
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+    });
 
 export const ProjectsFeed = ({ feedSource }) => {
-  return (
-    <div className="main-container--text-container">
-      <section className="text-container--projects">
-        <div className="projects--container">
-          {feedSource.map(({ node }) => {
-            return <ProjectItem project={node} key={node.slug} />
-          })}
-        </div>
-      </section>
-    </div>
-  )
-}
+    return (
+        <section className="text-container--projects">
+            <div className="projects--container">
+                {feedSource.map(({ node }) => {
+                    return <ProjectItem project={node} key={node.slug} />;
+                })}
+            </div>
+        </section>
+    );
+};
