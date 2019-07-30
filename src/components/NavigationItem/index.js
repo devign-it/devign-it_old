72fb7272eb908fb143f37dae5f6e2adf104179bc
rubennijhuis <contact@rubenswebsite.com>
2019-07-30 21:12:17 +0200

    🧹 ref: dividing items from nav into new component

---
+++ src/components/NavigationItem/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link } from "gatsby";
+
+import { NavigationItemContainer } from "./styles";
+
+const NavigationItem = ({ text, to }) => (
+    <NavigationItemContainer className="main-navigation--item">
+        <h2>
+            <Link activeClassName={"isActive"} to={`/${to}/`}>
+                {text}
+            </Link>
+        </h2>
+    </NavigationItemContainer>
+);
+
+export default NavigationItem;
