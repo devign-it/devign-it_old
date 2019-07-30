6c5aef00e02c7b614014e34ae2eeb8b89a030324
rubennijhuis <contact@rubenswebsite.com>
2019-07-30 21:12:58 +0200

    🧹 refact: adding styling for nav items

---
+++ src/components/NavigationItem/styles.js
@@ -0,0 +1,51 @@
+import styled from "styled-components";
+
+/*
+    height: $s-main / 1.5;
+    background-color: $c-black;
+    border-right: 1.5px solid $c-white;
+
+    background-color: $c-white;
+    color: $c-black;
+*/
+
+export const NavigationItemContainer = styled.div`
+    width: calc(100% / 3);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    overflow: hidden;
+    height: calc(8rem / 1.5);
+    background-color: black;
+    border-right: 1.5px solid white;
+    border-color: white;
+
+    /* @media screen and (min-width: $tablet-breakpoint) {
+        height: $s-main;
+        padding: 0;
+        background-color: inherit;
+        border-right: 3px solid $c-white;
+    } */
+
+    h2 {
+        padding: 0;
+        margin: 0;
+        text-align: center;
+        transform: translate3d(0, 0, 0) rotateX(0) rotateY(0) rotateZ(0);
+        transform-style: preserve-3d;
+    }
+
+    &:after,
+    &:before {
+        content: none;
+    }
+
+    &:last-child {
+        border-right: 0;
+    }
+
+    &.isActive {
+        background-color: white;
+        color: black;
+    }
+`;
